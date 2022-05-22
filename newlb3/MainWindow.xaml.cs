19e6a6dcc7b03fe1b5df03f3e83b7917d87d250f
Gorgy <egorsasim@mail.ru>
2022-05-22 17:38:20 +0300

    add great posibility to add certain class and check for valid inp

--- newlb3/MainWindow.xaml.cs
+++ newlb3/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using lb3;
+using lb3.Vehicles;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,61 @@ namespace newlb3
         public MainWindow()
         {
             InitializeComponent();
+           
+            cmbBoxSelectVehicle.Items.Add(new Boat());
+            cmbBoxSelectVehicle.Items.Add(new Bus());
+            cmbBoxSelectVehicle.Items.Add(new Car());
+            cmbBoxSelectVehicle.Items.Add(new Plane());
+            cmbBoxSelectVehicle.Items.Add(new Train());
+        }
+
+        List<Transport> vehicles = new List<Transport>();
+
+
+        public void showVehicles()
+        {
+            lstBoxVehicles.Items.Clear();
+            foreach (var item in vehicles)
+            {
+                lstBoxVehicles.Items.Add($"{item} {item.speed} {item.name} {item.id} {item.power} {item.places}");
+            }
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Create the class " + cmbBoxSelectVehicle.SelectedValue);
+
+            int id, places;
+            double power, speed;
+            string name;
+            if (Checks.checkEmpty(vehSpeed.Text, vehName.Text, vehId.Text, vehPower.Text, vehPlaces.Text))
+            {
+                if (Checks.ValidSpec(vehSpeed.Text, vehName.Text, vehId.Text, vehPower.Text, vehPlaces.Text))
+                {
+                    id = Convert.ToInt32(vehId.Text);
+                    places = Convert.ToInt32(vehPlaces.Text);
+                    power = Convert.ToDouble(vehPower.Text);
+                    speed = Convert.ToDouble(vehSpeed.Text);
+                    name = vehName.Text;
+
+                    Type t = Type.GetType(cmbBoxSelectVehicle.SelectedItem.ToString());
+                    Transport transport = (Transport)Activator.CreateInstance(t, speed, name, id, power, places);
+                    vehicles.Add(transport);
+
+
+                    showVehicles();
+                }
+                else
+                {
+                    MessageBox.Show("Not valid values, please do smth about it)");
+                }
+            }
+            else
+            {
+                MessageBox.Show("An empty fields detected!!!");
+            }
+
         }
     }
 }
